6aee8b518a714c98f3ed154d18a349bb976f0c76
Krishna Varshney <commercekv2003@gmail.com>
2023-04-01 13:28:26 +0530

    changes

--- basic_c++/sum_number_vector.cpp
+++ basic_c++/sum_number_vector.cpp
@@ -1,3 +1,25 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+int main(void){
+    vector<int>v1(4);
+    for (int i = 0; i <4; i++)
+    {
+        cin>>v1[i];
+    }
+    int num;
+    cout<<"Enter the number you want to check the sum:";
+    cin>>num;
+    bool flag=false;
+    for (int i = 0; i < v1.size(); i++)
+    {
+        if ((v1[i]+v1[i+1])==num)
+        {
+            flag=true;
+        }
+
+    }
+    cout<<flag;
+    
+
+}

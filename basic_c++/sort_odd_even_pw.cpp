3a6f38f2258c54412a0a99bb7d4dc6be19c1f338
Krishna Varshney <commercekv2003@gmail.com>
2023-04-02 16:49:25 +0530

    error

---
+++ basic_c++/sort_odd_even_pw.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(void)
+{
+    vector<int> v = {9, 8, 7, 6, 5, 3, 9, 1, 2};
+    int left_ptr=0;
+    int right_ptr=v.size()-1;
+    int temp=0;
+    while (left_ptr<right_ptr)
+    {
+        if (v[left_ptr]%2!=0&&v[right_ptr]%2==0)
+        {
+            // temp=v[left_ptr];
+            // v[left_ptr]=v[right_ptr];
+            // v[right_ptr]=temp;
+            swap(v[left_ptr],v[right_ptr]);
+            left_ptr++;
+            right_ptr--;
+        }
+        if (v[left_ptr]%2==0)
+        {
+            left_ptr++;
+        }
+        if (v[right_ptr]!=2)
+        {
+            right_ptr--;
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            cout<<v[i]<<" ";
+        }
+        
+        
+    }
+    
+
+}

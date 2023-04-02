91cc46e18c20022eb78772daaee59acd20562dbb
Krishna Varshney <commercekv2003@gmail.com>
2023-04-02 16:01:22 +0530

    my logic

--- basic_c++/sort_odd_even.cpp
+++ basic_c++/sort_odd_even.cpp
@@ -3,5 +3,29 @@
 #include<algorithm>
 using namespace std;
 int main(void){
-    
+    vector<int>v={1,2,3,4,5};
+    vector<int>v2;
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v[i]%2==0)
+        {
+            v2.push_back(v[i]);
+            v.erase(v.begin() + i);
+        }
+        
+    }
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout<<endl;
+    for (int i = 0; i <v.size() ; i++)
+    {
+        v2.push_back(v[i]);
+    }
+    for (int i = 0; i < v2.size(); i++)
+    {
+        cout<<v2[i]<<" ";
+    }
+
 }

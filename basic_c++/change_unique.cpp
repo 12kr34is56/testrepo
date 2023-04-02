46d3e94acf917889ca683d871d1f9d9091c7a286
Krishna Varshney <commercekv2003@gmail.com>
2023-04-02 13:48:15 +0530

    git commit -m "new change"

--- basic_c++/change_unique.cpp
+++ basic_c++/change_unique.cpp
@@ -16,7 +16,7 @@ int main(void){
         {
             if (v[i]!=v[j])
             {
-                num=v[i];
+                num=v[j];
                 break;
             }
             
--- basic_c++/rotate_array.cpp
+++ basic_c++/rotate_array.cpp
@@ -1,34 +1,49 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
 int main(void){
-    int size=6;
-    int arr[size];
-    int ansarr[size];
+    // int size=6;
+    // int arr[size];
+    // int ansarr[size];
     
-    for (int i = 0; i < size; i++)
-    {
-        cout<<"Enter the value:"<<i+1<<endl;
-        cin>>arr[i];
-    }
-    int number_rotation;
-    cout<<"Enter the times u want to rotate the array:"<<endl;
-    cin>>number_rotation;
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cout<<"Enter the value:"<<i+1<<endl;
+    //     cin>>arr[i];
+    // }
+    // int number_rotation;
+    // cout<<"Enter the times u want to rotate the array:"<<endl;
+    // cin>>number_rotation;
     
-    number_rotation=number_rotation%size;
-    int j=0;
+    // number_rotation=number_rotation%size;
+    // int j=0;
     
-    for (int i = size-number_rotation; i < size; i++)
+    // for (int i = size-number_rotation; i < size; i++)
+    // {
+    //     ansarr[j++]=arr[i];
+    // }
+    // for (int i = 0; i <= number_rotation ; i++)
+    // {
+    //     ansarr[j++]=arr[i];
+    // }
+    
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cout<< ansarr[i];
+    // }
+    vector<int>v={1,2,3,4,5};
+    int rotation=2;
+    rotation=rotation%v.size();
+    reverse(v.begin(),v.end());
+    reverse(v.begin(), v.end()-rotation);
+    reverse(v.begin()+rotation,v.end());
+    for (int i = 0; i < v.size(); i++)
     {
-        ansarr[j++]=arr[i];
-    }
-    for (int i = 0; i <= number_rotation ; i++)
-    {
-        ansarr[j++]=arr[i];
+        cout<<v[i]<<" ";
     }
     
-    for (int i = 0; i < size; i++)
-    {
-        cout<< ansarr[i];
-    }
+    
+       
 }
